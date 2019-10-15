05da2e11f4b520e317b6246a7c0ec25a12f7fdf6
allanbastos <allabatistabastos@gmail.com>
2019-10-15 15:58:35 -0300

    new folder

---
+++ cmake-build-debug/CMakeFiles/Curso_C__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Curso_C__.dir/main.cpp.o"
+  "Curso_C__.pdb"
+  "Curso_C__"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Curso_C__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
